321d68822df1541d1460b08d3438399ea3a75b27
Surajkumarsaw1 <44591689+Surajkumarsaw1@users.noreply.github.com>
2023-03-30 00:41:11 +0530

    5-d-20

--- assignment-5/d/20.c
+++ assignment-5/d/20.c
@@ -14,27 +14,51 @@ For an incorrect choice, an appropriate error message should be displayed.
 int main() {
     int op, n;
 
-    
-
     printf("Options : \n\t");
     printf("1. Generate and display the first 10 terms of the Fibonacci series. \n\t");
-    printf("2. ind the sum of the digits of an integer that is input.\n\n\t");
+    printf("2. Find the sum of the digits of an integer that is input.\n\n\t");
     printf("Enter option : ");
     scanf("%d",&op);
-
-    printf("Enter a number : ");
-    scanf("%d",&n);
 
     switch (op)
     {
     case 1:
-        
+        n = 10;
+        int f=0, s=1, next;
+
+        for (int i = 0; i < n; i++){
+            if ( i < 2) {
+                printf("%d, ",i);
+            }
+            else {
+                next = f + s;
+                if ( i != 9){
+                    printf("%d, ",next);
+                }
+                else{
+                    printf("%d\n",next);
+                }
+                f = s;
+                s = next;
+            }
+        }
 
         break;
     
     case 2:
-        
+        int sum = 0, num, count=0;
+        printf("Enter a number : ");
+        scanf("%d",&n);
+        num = n;
 
+        while ( n != 0){
+            count += 1;
+            sum +=  n%10;
+            // printf("%d\n",sum);
+            n /= 10;
+        }
+        printf("Sum of digits of %d : %d\n",num,sum);
+        // printf("count : %d",count);
         break;
 
     default:
