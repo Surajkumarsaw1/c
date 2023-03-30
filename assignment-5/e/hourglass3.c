70204e5a33978d099d81f66beab2e99edeb8a700
BillyDoesDev <64155209+BillyDoesDev@users.noreply.github.com>
2023-03-30 11:31:18 +0530

    Add files via upload

---
+++ assignment-5/e/hourglass3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j < i; j++)
+            printf("  ");
+        for (int j = 1; j <= 2 * (5 - i) + 1; j++)
+            printf("%c ", j + 64);
+        printf("\n");
+    }
+
+    for (int i = 2; i <= 5; i++)
+    {
+        for (int j = 5 - i; j >= 1; j--)
+            printf("  ");
+        for (int j = 1; j <= 2 * i - 1; j++)
+            printf("%c ", j + 64);
+        printf("\n");
+    }
+
+    return 0;
+}
