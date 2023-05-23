229b16fe9d3c0c863025e1fc92197fccccd4bf9a
Surajkumarsaw1 <44591689+Surajkumarsaw1@users.noreply.github.com>
2023-05-23 15:36:00 +0530

    modified:   assignment-8/6.c

--- assignment-8/6.c
+++ assignment-8/6.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 #include <math.h>
 
+int isArmstrong(int num);
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    float (*fp)(int);
+    fp = isArmstrong;
+    if (fp(num)) {
+        printf("%d is an Armstrong number.\n", num);
+    } else {
+        printf("%d is not an Armstrong number.\n", num);
+    }
+
+    return 0;
+}
+
 int isArmstrong(int num) {
     int count = 0, temp = num, sum = 0;
     
@@ -17,19 +34,4 @@ int isArmstrong(int num) {
     }
 
     return (sum == num);
-}
-
-int main() {
-    int num;
-    printf("Enter a number: ");
-    scanf("%d", &num);
-    float (*fp)(int);
-    fp = isArmstrong;
-    if (fp(num)) {
-        printf("%d is an Armstrong number.\n", num);
-    } else {
-        printf("%d is not an Armstrong number.\n", num);
-    }
-
-    return 0;
 }
